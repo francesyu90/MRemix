ed97a002c58e7d970489a079c905bdca82a8787b
seng299Project <francesyu90@yahoo.com>
2016-01-05 01:25:43 -0800

    Add search functionality to Library page

--- mremix/client/helpers.js
+++ mremix/client/helpers.js
@@ -1,9 +1,5 @@
 Meteor.subscribe("musicurls");
 
 Template.getFilesFromDatabase.helpers({
-
-	musicurls:function(){
-		return MusicUrls.find({});
-	}
-
+	musicUrlsIndex: () => MusicUrlsIndex
 });
--- mremix/lib/collections.js
+++ mremix/lib/collections.js
@@ -1,1 +1,6 @@
 MusicUrls = new Mongo.Collection("musicurls");
+MusicUrlsIndex = new EasySearch.Index({
+    collection: MusicUrls,
+    fields: ['musicName'],
+    engine: new EasySearch.Minimongo()
+  });
