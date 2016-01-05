d1b9cab161be55616ed58e0970c6f94b7a777adc
francesyu90 <francesyu90@yahoo.com>
2016-01-05 09:34:44 -0800

    Implement add tracks to or remove all tracks from playlist functionality

--- mremix/client/events.js
+++ mremix/client/events.js
@@ -1,3 +1,5 @@
+var playlist = [];
+
 Template.AddMusicModal.events({
 
 	"submit .js-add-music":function(event){
@@ -9,6 +11,24 @@ Template.AddMusicModal.events({
 			musicUrl:musicUrl
 		}
 		Meteor.call("addMusic", music);
+	}
+
+});
+
+Template.getFilesFromDatabase.events({
+
+	"click .js-tog-addAll":function(event){
+		var isAddAll = event.target.checked;
+		if(isAddAll){
+			var musicurls = MusicUrls.find({}).fetch();
+			playlist = [];
+			for(var i = 0; i < musicurls.length; i++){
+				playlist.push(musicurls[i]._id);
+			}
+		} else {
+			playlist = [];
+		}
+		Session.set("playlist", playlist);
 	}
 
 });
